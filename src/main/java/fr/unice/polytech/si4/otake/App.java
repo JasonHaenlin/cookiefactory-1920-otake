7960698370c83856e22b7ef03a36a6a62d67a1f5
Jason Haenlin <jasonjas5252@gmail.com>
2019-10-31 20:20:49 +0100

    GH-2 add a logger

--- src/main/java/fr/unice/polytech/si4/otake/App.java
+++ src/main/java/fr/unice/polytech/si4/otake/App.java
@@ -1,16 +1,20 @@
 package fr.unice.polytech.si4.otake;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App {
+    private static Logger logger = LogManager.getLogger(App.class);
 
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        logger.info(new App().getGreeting());
     }
 }
