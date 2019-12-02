3e92683fc845675ae0fde3e169fea669bdbd51be
davidbisegna <david.bisegna@etu.univ-cotedazur.fr>
2019-12-02 16:50:26 +0100

    Added very Basic Pack

---
+++ src/main/java/fr/unice/polytech/si4/otake/cookiefactory/product/Pack.java
@@ -0,0 +1,4 @@
+package fr.unice.polytech.si4.otake.cookiefactory.product;
+
+public class Pack {
+}
---
+++ src/main/java/fr/unice/polytech/si4/otake/cookiefactory/product/cookie/exception/IncorrectPackSize.java
@@ -0,0 +1,4 @@
+package fr.unice.polytech.si4.otake.cookiefactory.product.cookie.exception;
+
+public class IncorrectPackSize {
+}
